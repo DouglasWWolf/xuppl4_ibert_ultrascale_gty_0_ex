487a0f2044c1911869346c00dd5fb31f1f6af6c7
DouglasWWolf <douglaswolf@yahoo.com>
2024-05-28 14:51:23 -0700

    No change history kept

---
+++ connect.sh
@@ -0,0 +1,7 @@
+echo "# xuppl4_ibert_ultrascale_gty_0_ex" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/DouglasWWolf/xuppl4_ibert_ultrascale_gty_0_ex.git
+git push -u origin main
